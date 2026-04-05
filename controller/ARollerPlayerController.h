ce119edffee239f5f74efb37a51ff5856c6dc16e
AM <alm36@pct.edu>
2026-04-05 17:28:44 -0400

    Create ARollerPlayerController.h

---
+++ controller/ARollerPlayerController.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RollerPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROLLER_API ARollerPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	protected:
+        virtual void BeginPlay() override;
+        virtual void SetupInputComponent() override;
+    
+    public:
+        UPROPERTY(EditDefaultsOnly, Category="Input")
+        UInputMappingContext* DefaultMappingContext;
+    
+        UPROPERTY(EditDefaultsOnly, Category="Input")
+        UInputAction* MoveAction;
+    
+        UPROPERTY(EditDefaultsOnly, Category="Input")
+        UInputAction* LookAction;
+    
+        UPROPERTY(EditDefaultsOnly, Category="Input")
+        UInputAction* JumpAction;
+    
+    private:
+        void Move(const FInputActionValue& Value);
+        void Look(const FInputActionValue& Value);
+        void StartJump();
+        void StopJump();
+	
+};
