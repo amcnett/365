61ed4c81795a0d71062340fe76e5e5db80b2c7d2
AM <alm36@pct.edu>
2026-04-12 22:19:43 -0400

    Create weapon.cpp

---
+++ rob/weapon.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Weapon.h"
+
+// Sets default values
+AWeapon::AWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	OnActorBeginOverlap.AddDynamic(this, &AWeapon::OnWeaponBeginOverlap);
+}
+
+// Called every frame
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AWeapon::OnWeaponBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	UE_LOG(LogTemp, Log, TEXT("Weapon overlapped"));
+	//GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Orange, TEXT("Weapon overlapped"));
+	//UE_LOG(LogTemp, Log, TEXT("Weapon overlapped"));
+
+	auto character = Cast<ARollerCharacter>(OtherActor);
+
+	if (character != nullptr)
+	{
+		if (Holder == nullptr)
+		{
+			auto playerAvatar = Cast<ARollerCharacter>(character);
+
+			if (playerAvatar != nullptr)
+			{
+				Holder = character;
+				playerAvatar->AttachWeapon(this);
+			}
+		}
+	}
+}
+
